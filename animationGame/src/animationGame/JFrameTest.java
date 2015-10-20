e068a891cb832353b2d5240889d9d1856ab29069
Hollmann.Justin <justin.hollmann.jh@gmail.com>
2015-10-20 14:57:05 +0200

    JFrame

--- animationGame/src/animationGame/JFrameTest.java
+++ animationGame/src/animationGame/JFrameTest.java
@@ -6,6 +6,8 @@ public class JFrameTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			JFrame f = new JFrame("Hello world!!");
+			f.setSize(1000, 500);
+			f.setVisible(true);
 	}
 
 }
