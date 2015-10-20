77cd7e6b50d293c360a8dd6ad838852573f7c1d4
Hollmann.Justin <justin.hollmann.jh@gmail.com>
2015-10-20 15:33:05 +0200

    Logo

--- animationGame/src/animationGame/FirstPanel.java
+++ animationGame/src/animationGame/FirstPanel.java
@@ -3,20 +3,36 @@ import javax.swing.*;
 
 public class FirstPanel extends JPanel {
 
-	    int[] xPoints = {40, 60, 70, 80, 100, 80, 70, 60};
-	    int[] yPoints = {440, 430, 410, 430, 450, 470, 450, 460};
+	    int[] xPoints = {40, 50, 40, 80};
+	    int[] yPoints = {50, 65, 80, 65};
 	    int nPoints = xPoints.length;
+	    
+	    int[] x2Points = {80, 120, 110, 120};
+	    int[] y2Points = {65, 80, 65, 50};
+	    int n2Points = xPoints.length;
+	    
+	    int[] x3Points = {65, 80, 95, 80};
+	    int[] y3Points = {25, 65, 25, 35};
+	    int n3Points = xPoints.length;
+	    
+	    int[] x4Points = {65, 80, 95, 80};
+	    int[] y4Points = {105, 65, 105, 95};
+	    int n4Points = xPoints.length;
 	
 	@Override public void paintComponent(java.awt.Graphics g){
-		g.fillRect(30,50,45,80);
-		g.fillOval(60, 30, 80, 50);
-	//	g.drawPolygon(xPoints, yPoints, nPoints);
+		//g.fillRect(30,50,45,80);
+		//g.fillOval(60, 30, 80, 50);
+		g.drawPolygon(xPoints, yPoints, nPoints);
+		g.drawPolygon(x2Points, y2Points, n2Points);
+		g.drawPolygon(x3Points, y3Points, n3Points);
+		g.drawPolygon(x4Points, y4Points, n4Points);
 	}
 	
 	public static void main(String []args){
 		JFrame f=new JFrame();
 		f.add(new FirstPanel());
-		f.pack();
+		f.setSize(400, 250);
+		//f.pack();
 		f.setVisible(true);
 	}
 }
